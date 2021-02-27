c6efe0cf0a21c45a445e3dbc074825d99d767ec8
Fabricio Souza <fabricio.sistemas19@gmail.com>
2021-02-27 14:23:00 -0300

    Project Completed

---
+++ src/app/room-list/room-list.component.ts
@@ -0,0 +1,52 @@
+import { RoomDetailsComponent } from 'src/app/room-details/room-details.component';
+import { Observable } from "rxjs";
+import { RoomService } from "../room.service";
+import { Room } from "../room";
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: "app-room-list",
+  templateUrl: "./room-list.component.html",
+  styleUrls: ["./room-list.component.css"]
+})
+export class RoomListComponent implements OnInit {
+  rooms: Observable<Room[]>;
+  public paginaAtual = 1;
+
+  key: string = 'name'; // Define um valor padrão, para quando inicializar o componente
+    reverse: boolean = false;
+    sort(key) {
+        this.key = key;
+        this.reverse = !this.reverse;
+    }
+
+  constructor(private roomService: RoomService,
+    private router: Router) {}
+
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.rooms = this.roomService.getRoomsList();
+  }
+
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  roomDetails(id: number){
+    this.router.navigate(['details', id]);
+  }
+
+  updateRoom(id: number){
+    this.router.navigate(['update', id]);
+  }
+}
